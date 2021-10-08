cccb08761c72ccdb8325df1efd87e4685d26f859
Zachary Shorts <shorts23@gmail.com>
2021-10-07 23:53:29 -0700

    changed to matt learning to code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Matt is learning how to code
         </a>
       </header>
     </div>
